4f3f63b67931615fc11148826c48e6bf1174afa2
Fernando Valero <fernandovalero@MacBook-Air-de-Fernando.local>
2019-10-25 23:04:27 -0500

    home listo con karma

---
+++ src/components/Brand.js
@@ -0,0 +1,53 @@
+import React from "react";
+
+const Brand = props => {
+  return (
+    <>
+      {/*<!-- Start brand Area -->*/}
+      <section className="brand-area section_gap">
+        <div className="container">
+          <div className="row">
+            <a className="col single-img" href="#">
+              <img
+                className="img-fluid d-block mx-auto"
+                src="img/brand/1.png"
+                alt=""
+              />
+            </a>
+            <a className="col single-img" href="#">
+              <img
+                className="img-fluid d-block mx-auto"
+                src="img/brand/2.png"
+                alt=""
+              />
+            </a>
+            <a className="col single-img" href="#">
+              <img
+                className="img-fluid d-block mx-auto"
+                src="img/brand/3.png"
+                alt=""
+              />
+            </a>
+            <a className="col single-img" href="#">
+              <img
+                className="img-fluid d-block mx-auto"
+                src="img/brand/4.png"
+                alt=""
+              />
+            </a>
+            <a className="col single-img" href="#">
+              <img
+                className="img-fluid d-block mx-auto"
+                src="img/brand/5.png"
+                alt=""
+              />
+            </a>
+          </div>
+        </div>
+      </section>
+      {/*<!-- End brand Area --></a>*/}
+    </>
+  );
+};
+
+export default Brand;
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,68 +1,30 @@
-import React from 'react';
+import React from "react";
+import Banner from "../components/Banner";
+import Features from '../components/Features';
+import Category from '../components/Category';
+import Product from '../components/Product';
+import Deal from '../components/Deal';
+import Brand from '../components/Brand';
+import Related from "../components/Related";
 
 class HomePage extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {  }
-    }
-    render() { 
-        return (
-            <>
-                
-
-                {/*<!-- start banner Area -->*/}
-                <section className="banner-area">
-                <div className="container">
-                    <div className="row fullscreen align-items-center justify-content-start">
-                    <div className="col-lg-12">
-                        <div className="active-banner-slider owl-carousel">
-                        {/*<!-- single-slide -->*/}
-                        <div className="row single-slide align-items-center d-flex">
-                            <div className="col-lg-5 col-md-6">
-                            <div className="banner-content">
-                                <h1>Yoshini store <br />Collection!</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                                <div className="add-bag d-flex align-items-center">
-                                <a className="add-btn" href=""><span className="lnr lnr-cross"></span></a>
-                                <span className="add-text text-uppercase">Add to Bag</span>
-                                </div>
-                            </div>
-                            </div>
-                            <div className="col-lg-7">
-                            <div className="banner-img">
-                                <img className="img-fluid" src="img/banner/banner-img.png" alt="" />
-                            </div>
-                            </div>
-                        </div>
-                        {/*<!-- single-slide -->*/}
-                        <div className="row single-slide">
-                            <div className="col-lg-5">
-                            <div className="banner-content">
-                                <h1>Nike New <br />Collection!</h1>
-                                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-                                dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation.</p>
-                                <div className="add-bag d-flex align-items-center">
-                                <a className="add-btn" href=""><span className="lnr lnr-cross"></span></a>
-                                <span className="add-text text-uppercase">Add to Bag</span>
-                                </div>
-                            </div>
-                            </div>
-                            <div className="col-lg-7">
-                            <div className="banner-img">
-                                <img className="img-fluid" src="img/banner/banner-img.png" alt="" />
-                            </div>
-                            </div>
-                        </div>
-                        </div>
-                    </div>
-                    </div>
-                </div>
-                </section>
-                {/*<!-- End banner Area -->*/}
-            </>
-        );
-    }
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+  render() {
+    return (
+      <>
+        <Banner />
+        <Features />
+        <Category />
+        <Product />
+        <Deal />
+        <Brand />
+        <Related />
+      </>
+    );
+  }
 }
- 
+
 export default HomePage;
