ba0e324f7b334c946f53e52194bce57b4b88d9a1
Fernando Valero <fer.dev.valero@gmail.com>
2019-11-06 08:20:17 -0600

    Se corrigieron unos href

--- src/components/ProductArea.js
+++ src/components/ProductArea.js
@@ -38,12 +38,12 @@ const ProductArea = props => {
                 <h2>{props.price}</h2>
                 <ul className="list">
                   <li>
-                    <a className="active" href="#">
+                    <a className="active" href="/">
                       <span>Category</span> : Household
                     </a>
                   </li>
                   <li>
-                    <a href="#">
+                    <a href="/">
                       <span>Availibility</span> : In Stock
                     </a>
                   </li>
@@ -78,13 +78,13 @@ const ProductArea = props => {
                   </button>
                 </div>
                 <div className="card_area d-flex align-items-center">
-                  <a className="primary-btn" href="#">
+                  <a className="primary-btn" href="/">
                     Add to Cart
                   </a>
-                  <a className="icon_btn" href="#">
+                  <a className="icon_btn" href="/">
                     <i className="lnr lnr lnr-diamond"></i>
                   </a>
-                  <a className="icon_btn" href="#">
+                  <a className="icon_btn" href="/">
                     <i className="lnr lnr lnr-heart"></i>
                   </a>
                 </div>
